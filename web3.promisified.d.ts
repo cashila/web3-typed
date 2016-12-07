0fa5baebc544f8a7c987e8ce7f9ab5250c6efe7a
alesl <aleslekse@gmail.com>
2016-12-07 21:07:03 +0100

    Contract abi

--- web3.promisified.d.ts
+++ web3.promisified.d.ts
@@ -5,6 +5,7 @@
 
 declare module "web3" {
   import BigNumber from "bignumber.js";
+  import { Definition as AbiDefinition } from "web3/abi";
 
   type BlockNumberOrHash = number | string;
   type VersionCallback = (err: any, version: string) => void;
@@ -468,6 +469,8 @@ declare module "web3" {
     }
 
     export class Contract {
+      readonly abi: AbiDefinition;
+
       /**
        * Contract address
        */
@@ -1171,5 +1174,40 @@ declare module "web3" {
   }
 
   export = Web3;
+}
+
+declare module "web3/abi" {
+  export type UIntType = "uint8" | "uint16" | "uint24" | "uint32" | "uint40" | "uint48" | "uint56" | "uint64" | "uint72" | "uint80" | "uint88" | "uint96" | "uint104" | "uint112" | "uint120" | "uint128" | "uint136" | "uint144" | "uint152" | "uint160" | "uint168" | "uint176" | "uint184" | "uint192" | "uint200" | "uint208" | "uint216" | "uint224" | "uint232" | "uint240" | "uint248" | "uint256";
+  export type IntType = "int8" | "int16" | "int24" | "int32" | "int40" | "int48" | "int56" | "int64" | "int72" | "int80" | "int88" | "int96" | "int104" | "int112" | "int120" | "int128" | "int136" | "int144" | "int152" | "int160" | "int168" | "int176" | "int184" | "int192" | "int200" | "int208" | "int216" | "int224" | "int232" | "int240" | "int248" | "int256";
+  export type BytesType =  "bytes1" | "bytes2" | "bytes3" | "bytes4" | "bytes5" | "bytes6" | "bytes7" | "bytes8" | "bytes9" | "bytes10" | "bytes11" | "bytes12" | "bytes13" | "bytes14" | "bytes15" | "bytes16" | "bytes17" | "bytes18" | "bytes19" | "bytes20" | "bytes21" | "bytes22" | "bytes23" | "bytes24" | "bytes25" | "bytes26" | "bytes27" | "bytes28" | "bytes29" | "bytes30" | "bytes31" | "bytes32";
+  export type Type = UIntType | IntType | "address" | "bool" | BytesType;
+
+  export interface Argument {
+    name: string;
+    type: Type;
+  }
+
+  export interface Function {
+    constant: boolean;
+    inputs: Argument[];
+    name: string;
+    outputs: Argument[];
+    type: "function";
+  }
+
+  export interface Constructor {
+    inputs: Argument[];
+    type: "constructor";
+  }
+
+  export interface Event {
+    anonymous: boolean;
+    inputs: Argument[];
+    name: string;
+    type: "event";
+  }
+
+  export type DefinitionItem = Function | Constructor | Event;
+  export type Definition = DefinitionItem[];
 }
 
